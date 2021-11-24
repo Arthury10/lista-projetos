9b305e706e284387f466f7dbf774f73cc924a94c
unknown <arthurropke@hotmail.com>
2021-11-24 15:28:47 -0300

    ajustes bg e cores

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,11 @@
+import load from './modules/load.js'
 import pullImg from './modules/pullImg.js'
 import list from './modules/list.js'
+import particles from './modules/particles.js'
+import darkMode from './modules/darkMode.js'
 
+load()
 pullImg()
 list()
+particles()
+darkMode()
