fbfa8463446bf2f868c29382a86b38a40dc51b0f
unknown <arthurropke@hotmail.com>
2021-11-30 12:01:46 -0300

    add lista de projetos github

--- src/js/modules/list.js
+++ src/js/modules/list.js
@@ -7,15 +7,28 @@ export default function list() {
 
 	//nova lista
 	const novaLista = (title, description, imgHtml, imgCss, imgJs, link) => {
-		const html = `<div class="projetos">
+		//fazer verificação quando for null ou undefined mostrar uma descrição padrão
+		if (description === null || description === undefined) {
+			description = 'Sem descrição'
+		}
+		//fazer verificação quando for null ou undefined mostrar uma imagem padrão
+		if (
+			imgList.src === null ||
+			imgList.src === undefined ||
+			imgList.src === ''
+		) {
+			imgList.src = '../../../assets/projetos/default.png'
+		}
+
+		const html = `<div class="projetos repo-list">
   <img
     class="item-projeto"
     src=${imgList.src}
   />
   <div class="projeto-description">
-    <h3 class="portTitle">${title}</h3>
+    <h3 class="portTitle repo-name">${title}</h3>
 		<h1 class="btnDelete">X</h1>
-    <p class="portText">
+    <p class="portText repo-description">
       <span class="cor-2">></span> ${description}
     </p>
     <div class="tecnologia-usada-pack">
@@ -23,7 +36,7 @@ export default function list() {
         <span class="cor-2">></span> Tecnologias
         usadas
       </p>
-      <div class="tecnologias-usada">
+      <div class="tecnologias-usada repo-stack">
         ${imgHtml}
 				${imgCss}
 				${imgJs}
@@ -32,6 +45,7 @@ export default function list() {
     <a class="btnProjeto" target="_blanck" href="${link}">VER NO GITHUB</a>
   </div>
 </div>`
+
 		return html
 	}
 
@@ -132,14 +146,92 @@ export default function list() {
 		}
 	})
 
-	//carregar lista do localStorage
-	window.addEventListener('load', () => {
-		const listaProjetos = localStorage.getItem('listaProject')
-		if (listaProjetos) {
-			const lista = listaProjetos.split(';')
-			for (item of lista) {
-				localList.innerHTML += item
+	fetch('https://api.github.com/users/arthury10/repos')
+		.then(response => response.json())
+		.then(data => {
+			data.forEach(repo => {
+				const { name, description, html_url } = repo
+				const imgHtml =
+					repo.language !== 'HTML'
+						? `<svg
+						version="1.1"
+						xmlns="http://www.w3.org/2000/svg"
+						width="32"
+						height="32"
+						viewBox="0 0 1024 1024"
+					>
+						<title>arrow-up</title>
+						<g id="icomoon-ignore"></g>
+						<path
+							fill="var(--color-secondary)"
+							d="M64 0h896l-81.493 920.021-367.488 103.979-365.397-104.021zM363.989 416l-9.899-115.968 429.184 0.128 9.813-111.872-562.176-0.128 29.781 341.76h389.376l-13.909 146.176-124.16 34.304-126.080-34.56-8.021-90.027h-111.317l14.080 177.963 231.339 61.867 229.504-61.568 31.744-348.032z"
+						></path>
+					</svg>` //html
+						: ''
+				const imgCss =
+					repo.languages !== 'CSS'
+						? `<svg
+						version="1.1"
+						xmlns="http://www.w3.org/2000/svg"
+						width="32"
+						height="32"
+						viewBox="0 0 1024 1024"
+					>
+						<title>arrow-up</title>
+						<g id="icomoon-ignore"></g>
+						<path
+							fill="var(--color-secondary)"
+							d="M64 0h896l-81.493 920.021-367.488 103.979-365.44-104.021zM793.173 188.288l-562.347-0.128 9.088 111.872 432 0.085-10.88 115.883h-283.307l10.24 109.781h263.765l-15.616 150.315-124.16 34.304-126.123-34.56-8.021-90.027h-111.36l12.373 164.48 233.173 72.661 229.248-65.28z"
+						></path>
+					</svg>` //css
+						: ''
+				const imgJs =
+					repo.languages !== 'JavaScript'
+						? `<svg
+						version="1.1"
+						xmlns="http://www.w3.org/2000/svg"
+						width="32"
+						height="32"
+						viewBox="0 0 1024 1024"
+					>
+						<title>arrow-up</title>
+						<g id="icomoon-ignore"></g>
+						<path
+							fill="var(--color-secondary)"
+							d="M0 0h1024v1024h-1024zM940.117 779.776c-7.467-46.72-37.888-85.973-128.128-122.581-31.403-14.72-66.304-24.96-76.672-48.64-3.883-14.080-4.48-21.76-1.963-30.080 6.4-27.563 39.040-35.84 64.64-28.16 16.64 5.12 32 17.92 41.643 38.4 44.117-28.843 44.117-28.843 74.88-48-11.52-17.92-17.237-25.643-25.003-33.28-26.88-30.080-62.677-45.44-120.917-44.117l-30.080 3.797c-28.843 7.040-56.32 22.4-72.96 42.88-48.64 55.083-34.603 151.083 24.277 190.763 58.24 43.52 143.403 53.077 154.283 94.080 10.24 49.92-37.12 65.92-83.883 60.16-34.603-7.68-53.76-25.003-74.88-57.003l-78.080 44.843c8.96 20.48 19.2 29.397 34.56 47.317 74.24 74.923 259.84 71.083 293.163-42.837 1.237-3.84 10.24-30.080 3.157-70.4zM556.843 470.656h-95.915c0 82.688-0.384 164.864-0.384 247.68 0 52.565 2.688 100.821-5.888 115.669-14.080 29.397-50.347 25.643-66.816 20.48-16.896-8.363-25.472-19.883-35.413-36.48-2.688-4.48-4.693-8.363-5.419-8.363l-77.867 48c13.013 26.88 32 50.005 56.491 64.725 36.48 21.76 85.504 28.8 136.832 17.28 33.408-9.643 62.208-29.483 77.269-60.203 21.76-39.68 17.152-88.32 16.939-142.763 0.512-87.637 0-175.317 0-263.637z"
+						></path>
+					</svg>` //javascript
+						: ''
+
+				localList.innerHTML += novaLista(
+					name,
+					description,
+					imgHtml,
+					imgCss,
+					imgJs,
+					html_url
+				)
+			})
+		})
+
+	//input search list of repositories
+	const input = document.querySelector('#search')
+	input.addEventListener('keyup', function (e) {
+		let inputValue = e.target.value.toLowerCase()
+		let list = document.querySelectorAll('.repo-list')
+		list.forEach(item => {
+			let title = item.querySelector('.repo-name').innerText.toLowerCase()
+			let description = item
+				.querySelector('.repo-description')
+				.innerText.toLowerCase()
+			//tecnologias
+			if (title.indexOf(inputValue) != -1) {
+				item.style.display = 'flex'
+			} else if (description.indexOf(inputValue) != -1) {
+				item.style.display = 'flex'
+			} else {
+				item.style.display = 'none'
 			}
-		}
+		})
 	})
 }
--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,11 @@ import pullImg from './modules/pullImg.js'
 import list from './modules/list.js'
 import particles from './modules/particles.js'
 import darkMode from './modules/darkMode.js'
+import apiGitHub from './modules/apiGitHub.js'
 
 load()
 pullImg()
 list()
 particles()
 darkMode()
+apiGitHub()
